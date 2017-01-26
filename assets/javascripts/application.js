3b634632a1ae0a7c84c0189ee50ba4ab8689239b
Chippy <chippy@case.edu>
2017-01-26 00:59:18 -0500

    init

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3,10 +3,36 @@
 #= require 'bootstrap.js'
 #= require 'bootstrap.min.js'
 #= require 'jquery.fullPage.min.js'
+#= require 'morphext.js'
 #= require_tree ./
 #= require_self
 
 $(document).ready(function() {
+
+	$("#js-rotating-1").Morphext({
+			// The [in] animation type. Refer to Animate.css for a list of available animations.
+			animation: "bounceIn",
+			// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+			separator: ",",
+			// The delay between the changing of each phrase in milliseconds.
+			speed: 2000,
+			complete: function () {
+					// Called after the entrance animation is executed.
+			}
+	});
+
+	$("#js-rotating-2").Morphext({
+			// The [in] animation type. Refer to Animate.css for a list of available animations.
+			animation: "bounceIn",
+			// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
+			separator: ",",
+			// The delay between the changing of each phrase in milliseconds.
+			speed: 2000,
+			complete: function () {
+					// Called after the entrance animation is executed.
+			}
+	});
+
 	$('#fullpage').fullpage({
 
 		//Section Anchors
